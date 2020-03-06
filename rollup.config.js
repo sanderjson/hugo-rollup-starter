16fa22c8e04e9ded23d30c2f03595f2624f0eb54
sanderjson <sanderson.jma@gmail.com>
2020-03-05 19:28:41 -0700

    initial commit

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import commonjs from "@rollup/plugin-commonjs";
+import buble from "@rollup/plugin-buble";
+import replace from "@rollup/plugin-replace";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+const production = process.env.NODE_ENV === "production" ? true : false;
+
+export default {
+  input: "./src/assets/main.js",
+  output: {
+    file: "./src/assets/bundle/bundle.js",
+    format: "umd"
+  },
+  watch: {
+    clearScreen: false
+  },
+  plugins: [
+    commonjs(),
+    buble({
+      exclude: "node_modules/**"
+    }),
+    replace({
+      exclude: "node_modules/**",
+      ENV: JSON.stringify(process.env.NODE_ENV || "development")
+    }),
+    // !production &&
+    //   serve({
+    //     open: true,
+    //     openPage: "/",
+    //     contentBase: "/dist",
+    //     historyApiFallback: true,
+    //     historyApiFallback: "/200.html",
+    //     host: "localhost",
+    //     port: 1123,
+    //     headers: {
+    //       "Access-Control-Allow-Origin": "*",
+    //       foo: "bar"
+    //     }
+    //   }),
+    // !production &&
+    //   livereload({
+    //     watch: "dist",
+    //     verbose: true
+    //   })
+  ]
+};
---
+++ src/assets/js/test.js
@@ -0,0 +1,4 @@
+export default test;
+const test = () => {
+  return console.log("test");
+};
